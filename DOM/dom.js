4e938cbca853340c4b8c977d730aceb3f0d3942c
Ayushkumar302 <ayushxkumar2002@gmail.com>
2023-07-26 12:21:49 +0530

    Keyboard Events

--- DOM/dom.js
+++ DOM/dom.js
@@ -5,17 +5,17 @@
 // const myLi = document.all[19]
 // console.log(myLi)
 
-console.dir(document)
-console.log(document.URL)
-console.log(document.all)
+// console.dir(document)
+// console.log(document.URL)
+// console.log(document.all)
 // IDENTIFIER METHODS :-
 // 1. getElementById()
 // const myHeader = document.getElementById('header-title')
 // console.log(myHeader)
 
-const myHeading = document.all[4];
-document.all[4].textContent = "Welcome to DOM"
-console.log(myHeading)
+// const myHeading = document.all[4];
+// document.all[4].textContent = "Welcome to DOM"
+// console.log(myHeading)
 // // 2. getElementsByClassName
 // const listItems = document.getElementsByClassName('list-group-item')
 // console.log(listItems)
@@ -28,13 +28,13 @@ console.log(myHeading)
 // const myHeader = document.querySelector('form')
 // console.log(myHeader)
 
-console.log(document.querySelector('.list-group-item'))
+// console.log(document.querySelector('.list-group-item'))
 
-// 5. querySelectorAll
-console.log(document.querySelectorAll('.list-group-item'))
+// // 5. querySelectorAll
+// console.log(document.querySelectorAll('.list-group-item'))
 
-// DOM MANIPULATION METHODS :-
-const heading = document.querySelector('#header-title')
+// // DOM MANIPULATION METHODS :-
+// const heading = document.querySelector('#header-title')
 
 // 1. textContent -> text in b/w of the <> and </> of the elt
 // heading.textContent = 'Welcome to DOM';
@@ -46,7 +46,7 @@ const heading = document.querySelector('#header-title')
 // textContent changes the text b/w <> & </> while innerHTML adds a child to the given element
 
 // CSS with JavaScript (.style)
-const secondHeading = document.querySelector('#second-heading')
+// const secondHeading = document.querySelector('#second-heading')
 
 // JS Function - setTimeOut()
 // Syntax -> setTimeOut( callfn, timeoutDelay (in ms) )
@@ -65,42 +65,144 @@ const secondHeading = document.querySelector('#second-heading')
 
 
 
-const listItems = document.getElementsByClassName('list-group-item')
-for (let i = 0; i < listItems.length; i++) {
-    // odd items
-    if (i % 2 == 0) {
-        listItems[i].style.backgroundColor = 'lightblue'
-    }
+// const listItems = document.getElementsByClassName('list-group-item')
+// for (let i = 0; i < listItems.length; i++) {
+//     // odd items
+//     if (i % 2 == 0) {
+//         listItems[i].style.backgroundColor = 'lightblue'
+//     }
 
-}
+// }
 
-// EVENTS  in dom :-
-const mybtn = document.getElementById('btn')
+// **************** EVENTS  in dom************** :-
 
-//ES 5 syntax
-mybtn.addEventListener('click', () => {
+// element.addEventListner( eventName, () => {} )
+// element.addEventListner( eventName, fuctionName )
 
-    console.log('button clicked');
+// const mybtn = document.getElementById('btn')
 
-})
-// event dblclick - double click
-mybtn.addEventListener('dblclick', () => {
+// //ES 5 syntax
+// mybtn.addEventListener('click', () => {
 
-    console.log('button clicked');
+//     console.log('button clicked');
 
-})
+// })
+// // event dblclick - double click
+// mybtn.addEventListener('dblclick', () => {
+
+//     console.log('button clicked');
+
+// })
 
 
-document.getElementById('item-one').addEventListener('click' , () => 
+// document.getElementById('item-one').addEventListener('click' , () => 
+// {
+//     console.log('item pone was clicked');
+// }
+// )
+
+// // event  submit button  // it reloads the page again and again by clicking
+// document.getElementById('submitBtn').addEventListener('click' , () =>  
+// {
+//     console.log('submit button clicked');
+// })
+
+// event parameter - meta data of an event
+// document.getElementById('submitBtn').addEventListener('click' , (params) =>    //parameter can be of any name
+// {
+//     params.preventDefault()
+//     console.log('submit button clicked'); 
+// })
+
+
+// ****mouse  event****
+// mouseenter
+// document.getElementById('text-input').addEventListener('mouseenter' , ()=>
+// {
+
+//     console.log('cursor entered  the element');
+// }
+
+
+//)
+// mouseleave
+// document.getElementById('text-input').addEventListener('mouseenter' , ()=>
+// {
+
+//     console.log('cursor  left the element');
+// }
+
+
+// )
+
+// *****focus event*****
+// document.getElementById('text-input').addEventListener('focus' , ()=>
+// {
+
+//     console.log('element was focused!');
+// }
+
+
+//)
+//********keyborad events******
+// copy event
+// document.getElementById('text-input').addEventListener('copy'  , ()=>
+
+// {
+//     console.log('data was copied');
+// })
+
+// // cut event
+// document.getElementById('text-input').addEventListener('cut'  , ()=>
+
+// {
+//     console.log('data was cut');
+// })
+
+// // paste event
+// document.getElementById('text-input').addEventListener('paste'  , ()=>
+
+// {
+//     console.log('data was pasted');
+// })
+
+
+// keydown event
+// document.getElementById('text-input').addEventListener('keydown'  , ()=>
+
+// {
+//     console.log('key is down');
+// })
+
+// //keyup event
+// document.getElementById('text-input').addEventListener('keyup'  , ()=>
+
+// {
+//     console.log('key is up');
+// })
+
+//keyuppress event
+// document.getElementById('text-input').addEventListener('keypress'  , ()=>
+
+// {
+//     console.log('key is pressed');
+// })
+
+
+
+// 
+// document.getElementById('text-input').addEventListener('keypress'  , (e)=>
+
+// {
+//     // console.log(e);
+//     console.log(e.target.value);
+
+// })
+// alternavtive  -  for if user add any detail to text box and give its value after user finishes entering data
+document.getElementById('submitBtn').addEventListener('click' , (e)=>
 {
-    console.log('item pone was clicked');
-}
-)
+    e.preventDefault()
+    console.log( document.getElementById('text-input').value )
+  
 
-// event  submit button  // it reloads the page again and again by clicking
-document.getElementById('submitBtn').addEventListener('click' , () =>  
-{
-    console.log('submit button clicked');
 })
-
-// keyborad events
